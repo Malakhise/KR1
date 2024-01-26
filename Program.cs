329910f8eb62385ff7046909975f40009a2b286b
Sergey <Malahin.s@gmail.com>
2024-01-26 18:46:25 +0300

    создал функцию FillArray, заполнение массива

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+string[] FillArray(string [] array)
+{
+    int index = 0;
+    while (index < array.Length)
+    {
+        Console.Write("Введите строку: ");
+        array[index] = Console.ReadLine()!;
+        index++;
+    }
+    return array;
+}
+
+Console.Clear();
+Console.Write("Введите число элементов массива: ");
+int countElements =int.Parse(Console.ReadLine()!);
+string [] array = new string[countElements];
+Console.WriteLine($"[{string.Join(", ", FillArray(array))}]");

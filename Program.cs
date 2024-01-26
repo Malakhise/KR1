e05b9e24e25d0c5c9d2855e1cfc94b9cb4351557
Sergey <Malahin.s@gmail.com>
2024-01-26 18:48:09 +0300

    создал функцию NewArray, создание нового массива

--- Program.cs
+++ Program.cs
@@ -11,8 +11,30 @@ string[] FillArray(string [] array)
     return array;
 }
 
+string[] NewArray(string [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) 
+            count++; 
+    }
+string [] newArray = new string[count];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) 
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
+
 Console.Clear();
 Console.Write("Введите число элементов массива: ");
 int countElements =int.Parse(Console.ReadLine()!);
 string [] array = new string[countElements];
 Console.WriteLine($"[{string.Join(", ", FillArray(array))}]");
+Console.WriteLine($"[{string.Join(", ", NewArray(array))}]"); 
